53fcec7bcc11b3e70b18847f2160e2b269924102
Sabrinale <lengocphuongthao.90@gmail.com>
2017-11-11 00:29:58 +0200

    try

--- services/passport.js
+++ services/passport.js
@@ -19,8 +19,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
+    callbackURL: 'https://floating-anchorage-28249.herokuapp.com/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
     // console.log('access token: ', accessToken);
     // console.log('refreshToken: ', refreshToken);
