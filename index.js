135c74176d56bed446407c3e873c0b9493d9f732
Sabrinale <lengocphuongthao.90@gmail.com>
2017-11-10 22:41:21 +0200

    second add

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('./routes/authRoutes')(app);
 
+app.get('/', (req, res) => res.send('Hello World!'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost: ${PORT}`);
