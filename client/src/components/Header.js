1dd77a247ee69fbe94beb48258951a0e5c7df294
Sabrinale <lengocphuongthao.90@gmail.com>
2017-11-13 00:21:14 +0200

    c7 -74

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav>
+          <div className ="nav-wrapper">
+              <a className="left brand-logo">Emaily</a>
+         
+          <ul className="right">
+              <li>
+                  <a> Login with Google </a>
+              </li>
+              <li>
+                  <a> Log out </a>
+              </li>
+          </ul>
+          </div>
+      </nav>
+    )
+  }
+}
--- services/passport.js
+++ services/passport.js
@@ -21,23 +21,19 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback', 
     proxy : true
-}, (accessToken, refreshToken, profile, done) => {
+}, async (accessToken, refreshToken, profile, done) => {
     // console.log('access token: ', accessToken);
     // console.log('refreshToken: ', refreshToken);
-     console.log('profile: ', profile); 
      //id from profile object
     //without.save model instance object stays only in express api not in mongo
     // moi lan refresh thi lai luu id nen ktra ko can dk user moi 
     // tat ca ca dong code chung ta khoi tao asyn action
     //query return a promise
-    User.findOne({ googleId: profile.id })
-    .then(existingUser => {
+    const existingUser = await User.findOne({ googleId: profile.id });
         if (existingUser) {
-             done(null, existingUser);
-        } else { 
-    new User({ googleId: profile.id }).save()
-    .then(user => done(null, user));
-        }
-    });
+            return done(null, existingUser);
+        } 
+        const user = await new User({ googleId: profile.id }).save();
+             done(null, user);
 }
 ));
